3a1da0982ffa8e13bfc690c12808a6023900c45b
Dexter Crago <jodieanddexter@gmail.com>
2024-05-24 14:37:52 -0600

    Add portrait image to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from './components/Header/Header';
-import './App.css';
+import Portrait from './assets/portrait.png';
+import styles from './App.module.css';
 
 function App() {
   return (
     <>
       <Header />
+      <div className={styles.main}>
+        <img src={Portrait} alt="Dexter Crago" />
+      </div>
     </>
   );
 }
