98dec19808c3ada93b492a107e1d8833c87faee9
Dexter Crago <jodieanddexter@gmail.com>
2024-05-25 08:48:11 -0600

    Style Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,20 @@
+import Icon from '@mdi/react';
+import { mdiCircleSmall } from '@mdi/js';
 import styles from './Header.module.css';
 
 function Header() {
   return (
-    <div>
+    <div className={styles.header}>
       <h1>
-        <a className={styles.link} href="/">
+        <a className={styles.h1} href="/">
           Dexter Crago
         </a>
       </h1>
-      <nav className={styles.links}>
+      <nav>
         <a className={styles.link} href="/portfolio">
           Portfolio
         </a>
+        <Icon className={styles.dot} path={mdiCircleSmall} size={1.5} />
         <a className={styles.link} href="/resume">
           Resume
         </a>
