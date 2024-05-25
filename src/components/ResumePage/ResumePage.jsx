9528053aa0573d07df2bf6d529876b84a003587b
Dexter Crago <jodieanddexter@gmail.com>
2024-05-25 13:10:29 -0600

    Rough in ResumePage content and styling

--- src/components/ResumePage/ResumePage.jsx
+++ src/components/ResumePage/ResumePage.jsx
@@ -1,17 +1,28 @@
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 import styles from './ResumePage.module.css';
 
 function ResumePage() {
   return (
     <>
       <Header />
+      <hr />
       {/* Include both information as HTML and a link to download a pdf */}
-      <div className={styles.resume}>
-        <p>(NOT COMPLETED: Resume)</p>
+      <div className={styles.main}>
         <p>
-          (Once I have finished my resume I will come back and include it here)
+          (NOT COMPLETED: Resume - Once I have finished my resume I will come
+          back and include it here)
+        </p>
+        <hr />
+        <p>
+          If you&rsquo;d prefer to have the pdf version:{' '}
+          <a className={styles.link} href="../../assets/resume.pdf">
+            Download my Resume
+          </a>
         </p>
       </div>
+      <hr />
+      <Footer />
     </>
   );
 }
